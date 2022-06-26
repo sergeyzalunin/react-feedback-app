0d00f16e22bf20afaf7d4ef3dda27382dd752f98
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2022-06-26 11:59:00 +0300

    clean up

--- src/components/FeedBackItem.js
+++ src/components/FeedBackItem.js
@@ -5,17 +5,10 @@ function FeedBackItem() {
     const [text, setText] = useState(
         'This is an example of a feedback item')
 
-    const handleClick = () => {
-        setRating((prevRating) => {
-            return prevRating + 1
-        })
-    }
-
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
-            <button onClick={handleClick}>Click</button>
         </div>
     )
 }
