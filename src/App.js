e6b4c39242966078abdc0493f3f2b761ccd18b96
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2022-06-26 23:15:27 +0300

    add feedback form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedBackList from './components/FeedBackList'
 import FeedBackData from './data/FeedBackData'
 import FeedBackStats from './components/FeedBackStats'
+import FeedBackForm from './components/FeedBackForm';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedBackData)
@@ -17,6 +18,7 @@ function App() {
         <>
             <Header />
             <div className="container">
+                <FeedBackForm />
                 <FeedBackStats feedback={feedback} />
                 <FeedBackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
