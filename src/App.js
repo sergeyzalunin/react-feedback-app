9f4aca138bab2d503e36c24b27660c6c0f52a63e
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2022-06-26 11:24:30 +0300

    update index.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
+import Header from "./components/Header";
+
 function App() {
-    const title = 'blog post'
-    const body = 'This is my blog post'
-    const comments = [
-        { id: 1, text: "comment #1"},
-        { id: 2, text: "comment #2"},
-        { id: 2, text: "comment #3"}
-    ]
-
-    const loading = false
-    const showComments = true
-
-    if (loading) return <h1>Loading...</h1>
-
-    const commentsBlock = (
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    )
-
     return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1> 
-            <p>{body}</p>
+        <>
+            <Header text='sdsd'/>
 
-            {showComments && commentsBlock}
-        </div>
-        
+            <div className="container">
+                <h1>App</h1> 
+            </div>
+        </>
     )
 }
 
