43ba384369fa08143aa85ad75b337a39709b024a
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2022-06-26 23:05:40 +0300

    add feedback stats

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import Header from "./components/Header";
+import { useState } from 'react'
+import Header from "./components/Header"
 import FeedBackList from './components/FeedBackList'
-import FeedBackData from './data/FeedBackData';
+import FeedBackData from './data/FeedBackData'
+import FeedBackStats from './components/FeedBackStats'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedBackData)
@@ -14,9 +15,9 @@ function App() {
 
     return (
         <>
-            <Header text='header text'/>
-
+            <Header />
             <div className="container">
+                <FeedBackStats feedback={feedback} />
                 <FeedBackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
